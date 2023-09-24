2617b08efa94421cc9d5d02c446f0cb1eae87cd8
OlgaKuvich <olga.kuvichinskaya@mail.ru>
2023-09-24 23:25:34 +0300

    Д/з № 6, задачи 1-6

---
+++ hw6/script.js
@@ -0,0 +1,59 @@
+//Задание 1
+
+const box = [1, 5, 4, 10, 0, 3];
+
+    for (let i = 0; i < box.length; i++) {
+        if (box[i] == 10) break; 
+        console.log(box[i]);
+    }
+
+//Задание 2
+
+const num = [1, 5, 4, 10, 0, 3];
+console.log(num.indexOf(4));
+
+//Задание 3
+
+const item = [1, 3, 5, 10, 20];
+console.log(item.join(' '));
+
+//Задание 4
+
+let array = [];
+
+for (let i = 0; i < 3; i++) {
+	array[i] = []; 
+	
+	for (let j = 0; j < 3; j++) {
+		array[i].push(1); 
+	}
+}
+
+console.log(array);
+
+//Задание 5
+
+const number = [1, 1, 1];
+
+for (let i = 0; i < 3; i++) {
+    number.push(1+1);
+}
+console.log(number);
+
+//Задание 6
+const num2 = [9, 8, 7, 'a', 6, 5];
+console.log(num2.sort().pop());
+
+//Задание 7
+
+//Задание 8
+
+//Задание 9
+
+//Задание 10
+
+//Задание 11
+
+//Задание 12
+
+//Задание 13
